70bb0f08d6c2aa60f64f436e2cac4ef315782f97
r0pee <r0pee@r0pee.hu>
2025-04-30 15:49:16 +0000

    Consolidated temperature.cpp

--- Firmware/temperature.cpp
+++ Firmware/temperature.cpp
@@ -1133,14 +1133,6 @@ FORCE_INLINE static void soft_pwm_core()
 #endif // HEATER_BED_PIN > -1
 #endif // SLOW_PWM_HEATERS
 
-#if defined(OLD_BED_HEATER) && HEATER_BED_PIN > -1
-  static unsigned char soft_pwm_b;
-#ifdef SLOW_PWM_HEATERS
-  static unsigned char state_heater_b = 0;
-  static unsigned char state_timer_heater_b = 0;
-#endif
-#endif
-
 #ifndef SLOW_PWM_HEATERS
   /*
    * standard PWM modulation
@@ -1157,32 +1149,23 @@ FORCE_INLINE static void soft_pwm_core()
     } else WRITE(HEATER_0_PIN,0);
   }
 
-#if defined(HEATER_BED_PIN) && HEATER_BED_PIN > -1
+/* MKS */
+#if defined(OLD_BED_HEATER) && defined(HEATER_BED_PIN) && HEATER_BED_PIN > -1
+  
+  static unsigned char soft_pwm_b;
 
-#if 0  // @@DR vypnuto pro hw pwm bedu
-  // tuhle prasarnu bude potreba poustet ve stanovenych intervalech, jinak nemam moc sanci zareagovat
-  // teoreticky by se tato cast uz vubec nemusela poustet
-  if ((pwm_count & ((1 << HEATER_BED_SOFT_PWM_BITS) - 1)) == 0)
+  if (pwm_count == 0)
   {
-    soft_pwm_b = soft_pwm_bed >> (7 - HEATER_BED_SOFT_PWM_BITS);
-#ifndef SYSTEM_TIMER_2
-	// tady budu krokovat pomalou frekvenci na automatu - tohle je rizeni spinani a rozepinani
-	// jako ridici frekvenci mam 2khz, jako vystupni frekvenci mam 30hz
-	// 2kHz jsou ovsem ve slysitelnem pasmu, mozna bude potreba jit s frekvenci nahoru (a tomu taky prizpusobit ostatni veci)
-	// Teoreticky bych mohl stahnout OCR0B citac na 6, cimz bych se dostal nekam ke 40khz a tady potom honit PWM rychleji nebo i pomaleji
-	// to nicemu nevadi. Soft PWM scale by se 20x zvetsilo (no dobre, 16x), cimz by se to posunulo k puvodnimu 30Hz PWM
-	//if(soft_pwm_b > 0) WRITE(HEATER_BED_PIN,1); else WRITE(HEATER_BED_PIN,0);
-#endif //SYSTEM_TIMER_2
+    soft_pwm_b = soft_pwm_bed;
+    if(soft_pwm_b > 0) WRITE(HEATER_BED_PIN,1); else WRITE(HEATER_BED_PIN,0);
   }
-#elif defined(OLD_BED_HEATER)
- if (pwm_count == 0)
- {
-   soft_pwm_b = soft_pwm_bed;
-   if(soft_pwm_b > 0) WRITE(HEATER_BED_PIN,1); else WRITE(HEATER_BED_PIN,0);
- }
 
+  if(soft_pwm_b < pwm_count)
+  {
+    WRITE(HEATER_BED_PIN,0);
+  }
 #endif
-#endif
+/* MKS */
 
 #ifdef FAN_SOFT_PWM
   if ((pwm_count & ((1 << FAN_SOFT_PWM_BITS) - 1)) == 0)
@@ -1198,13 +1181,6 @@ FORCE_INLINE static void soft_pwm_core()
     WRITE(HEATER_1_PIN,0);
 #endif
   }
-
-#if defined(OLD_BED_HEATER) && defined(HEATER_BED_PIN) && HEATER_BED_PIN > -1
-  if(soft_pwm_b < pwm_count)
-  {
-    WRITE(HEATER_BED_PIN,0);
-  }
-#endif
 
 #ifdef FAN_SOFT_PWM
   if (soft_pwm_fan < (pwm_count & ((1 << FAN_SOFT_PWM_BITS) - 1))) WRITE(FAN_PIN,0);
